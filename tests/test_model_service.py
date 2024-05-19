f0a7a57fa17b6170cdf0d66fcf7929558c2497fd
Tip ten Brink <T.M.tenBrink@student.tudelft.nl>
2024-05-19 15:45:17 +0200

    start with model service docker

---
+++ tests/test_model_service.py
@@ -0,0 +1,26 @@
+from typing import Generator
+import httpx
+from  httpx import Client
+import pytest
+
+MODEL_SERVICE_URL = "http://localhost:5099"
+
+@pytest.fixture
+def service_client() -> Generator[Client, None, None]:
+    yield Client(base_url=MODEL_SERVICE_URL)
+
+def test_service(service_client: Client):
+    r = service_client.post("/predict", json={'url': 'tudelft.nl'})
+    assert r.status_code == 200
+    result_json = r.json()
+    assert 'score' in result_json
+    assert isinstance(result_json['score'], list)
+    assert len(result_json['score']) == 1
+
+def test_service_two(service_client: Client):
+    r = service_client.post("/predict", json={'url': ['tudelft.nl', 'google.com']})
+    assert r.status_code == 200
+    result_json = r.json()
+    assert 'score' in result_json
+    assert isinstance(result_json['score'], list)
+    assert len(result_json['score']) == 2
