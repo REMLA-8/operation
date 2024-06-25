3ef96e9806b6ac00c4fb824f776c195d4814d79a
Tip ten Brink <T.M.tenBrink@student.tudelft.nl>
2024-06-25 11:16:20 +0200

    istio install script with wait

--- k8s/istio_deploy.sh
+++ k8s/istio_deploy.sh
@@ -4,7 +4,8 @@ export KUBECONFIG=k3s.yaml
 
 helm upgrade --install metallb metallb \
   --repo https://metallb.github.io/metallb \
-  --namespace metallb-system --create-namespace
+  --namespace metallb-system --create-namespace --wait
 
 # Make loadbalancer available
 kubectl apply -f metal-pool.yml
+
